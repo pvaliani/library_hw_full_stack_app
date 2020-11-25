5279faece5cafdad77c8231056a95b8672c4f7a8
Pedram Valiani <p.valiani@gmail.com>
2020-11-25 20:29:40 +0000

    added delete controller

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -13,20 +13,23 @@ books_blueprint = Blueprint("books", __name__)
 
 # - To get books we can use a blueprint which allows us to add multiple paths to our books route on the app. I.e extensions to /books such as /books/new.html etc. Books is the "index" of our route??
 
+# - This blueprint for the route /"books" on the app. It returns the value of all_books which is a select all SQL query and for the books/index.html file in the templates
+
 @books_blueprint.route("/books")
 def books():
 
+    # - Python converted SQL query
     books = book_repository.select_all()
-
+    # - Return page with result of all books so we can loop through the data
     return render_template("books/index.html", all_books = books)
 
 # NEW
 # GET '/books/new'
 
-# @books_blueprint.route("/books/new", methods=['GET'])
-# def new_book():
-#     authors = author_repository.select_all()
-#     return render_template("books/new.html", all_authors = authors)
+@books_blueprint.route("/books/new", methods=['GET'])
+def new_book():
+    authors = author_repository.select_all()
+    return render_template("books/new.html", all_authors = authors)
 
 
 
@@ -49,4 +52,16 @@ def books():
 
 # DELETE
 # DELETE '/books/<id>'
+
+# - Here we define the action (also a route) we want to take on a book by its ID
+
+
+@books_blueprint.route("/books/<id>/delete", methods=["POST"])
+# - Ensure we give delete book id as an argument otherwise will get a TypeError
+def delete_book(id):
+    # - Python SQL query to delete by id
+    book_repository.delete(id)
+    # - refresh the page on books after we have deleted the item
+    return redirect("/books")
+    # - On index.html within books we create a POST form and button linked to action a delete from the page
 
