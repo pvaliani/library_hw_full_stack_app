c3dd464bce5c80ef917a2782f7d6a9e648b1367d
Pedram Valiani <p.valiani@gmail.com>
2020-11-25 20:04:58 +0000

    can view all books and their details

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-# TODO: import books blueprint here
+from controllers.books_controller import books_blueprint
 
 app = Flask(__name__)
 
-# TODO: register books blueprint here
+app.register_blueprint(books_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, Blueprint, redirect
 from models.book import Book
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository
 
+
+# - First create a blueprint
+
+books_blueprint = Blueprint("books", __name__)
+
 # INDEX
 # GET '/books'
 
+# - To get books we can use a blueprint which allows us to add multiple paths to our books route on the app. I.e extensions to /books such as /books/new.html etc. Books is the "index" of our route??
+
+@books_blueprint.route("/books")
+def books():
+
+    books = book_repository.select_all()
+
+    return render_template("books/index.html", all_books = books)
+
 # NEW
 # GET '/books/new'
+
+# @books_blueprint.route("/books/new", methods=['GET'])
+# def new_book():
+#     authors = author_repository.select_all()
+#     return render_template("books/new.html", all_authors = authors)
+
 
 
 # CREATE
